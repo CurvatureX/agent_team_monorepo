bff74cdd90b9bbefd990bcdfb2ac3959d6489926
JINGWEI ZHANG <z1771485029@gmail.com>
2025-07-16 21:02:21 +0800

    Update docusaurus.config.ts

--- apps/internal-tools/docusaurus-doc/docusaurus.config.ts
+++ apps/internal-tools/docusaurus-doc/docusaurus.config.ts
@@ -15,10 +15,8 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://curvaturex.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/agent_team_monorepo/',
+  url: 'https://docs.starmates.ai',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
